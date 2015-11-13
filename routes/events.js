5f3ffae78c638953f4821f74fcefc88212f17190
Helen Triolo <helentriolo@localfirstmedia.com>
2015-11-13 16:15:10 -0500

    Added JSON file with /JSON test and /events ejs update to show list

--- routes/events.js
+++ routes/events.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
 
 /* GET events page. TODO: show list of events and add form for a new one */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Event List' });
+  var obj = JSON.parse(fs.readFileSync('./jsondata/events.json', 'utf8'));
+  res.render('events/index', { data:obj, title:'Upcoming Events' });
 });
+
+router.get('/json', function(req, res) {
+  var obj = JSON.parse(fs.readFileSync('./jsondata/events.json', 'utf8'));
+  res.json(obj);
+})
 
 module.exports = router;
